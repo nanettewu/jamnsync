14d11d08682ae030f40e79e7c799a0e209fa8480
Nanette Wu <nanette.wu@gmail.com>
2021-01-26 16:26:48 -0500

    remove commented out routes (now in routes.py)

--- server/audio_processing/app.py
+++ server/audio_processing/app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import configparser
 
-
 config = configparser.ConfigParser()
 config.read('database.ini')
 
@@ -29,34 +28,3 @@ from audio_processing.models import User, RehearsalGroup, group_membership, Proj
 with app.app_context():
   from . import routes
 
-# @app.route('/')
-# def entry_point():
-#     return 'Hello World!'
-
-# @app.route('/test')
-# def test():
-#   return {"test": "/test successful!"}
-
-# # lists all files in s3 bucket
-# @app.route("/storage")
-# def storage():
-#   contents = list_files(BUCKET)
-#   return str(contents)
-
-# # uploads file to upload/ directory in s3 bucket
-# @app.route('/upload',methods=['post'])
-# def upload():
-#     if request.method == 'POST':
-#       f = request.files['file']
-#       filename = f.filename
-#       f.save(os.path.join(UPLOAD_FOLDER, filename))
-#       upload_file(filename, BUCKET)
-#       return f"Uploaded file: {filename}"
-
-# # downloads file from s3 bucket
-# @app.route('/download/<filename>',methods=['get'])
-# def download(filename):
-#     if request.method == 'GET':
-#       output = download_file(filename, BUCKET)
-#       # send file destination relative to audio_processing dir
-#       return send_file(output, as_attachment=True)
