df0c976add21d2c83bb692d609f7faf166c28428
Nanette Wu <nanette.wu@gmail.com>
2021-01-28 15:32:28 -0500

    refactor client file organization

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,44 @@
 import React, { Component } from "react";
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-
-import Footer from "./components/Footer"
-
-import Login from './components/Login';
-import Logout from './components/Logout';
-
-import Home from "./components/Home";
-import Projects from "./components/Projects";
-import AudioPlayer from "./components/AudioPlayer";
-import DAW from "./components/DAW";
-
-import PrivateRoute from "./utils/PrivateRoute";
+import { HashRouter, NavLink, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import AudioPlayer from "./routes/audioplayer/AudioPlayer"; // temp
+import DAW from "./routes/daw/DAW";
+import Home from "./routes/home/Home";
+import Projects from "./routes/projects/Projects";
+import PrivateRoute from "./utils/components/PrivateRoute";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       userDetails: {},
-      isUserLoggedIn: false
+      isUserLoggedIn: false,
     };
   }
 
-  handleSuccessfulLogin = response => {
+  handleSuccessfulLogin = (response) => {
     this.setState({ userDetails: response.profileObj, isUserLoggedIn: true });
   };
 
   handleSuccessfulLogout = () => {
-    this.setState({ isUserLoggedIn: false })
+    this.setState({ isUserLoggedIn: false });
   };
 
   render() {
     return (
       <HashRouter>
-        < div >
+        <div>
           <h1>JamNSync</h1>
-          <div style={{ position: 'absolute', right: 20, top: 35 }} >
+          <div style={{ position: "absolute", right: 20, top: 35 }}>
             {!this.state.isUserLoggedIn && (
               <Login onSuccessfulLogin={this.handleSuccessfulLogin} />
             )}
             {this.state.isUserLoggedIn && (
-              <div style={{ display: "flex" }} >
+              <div style={{ display: "flex" }}>
                 <div>
-                  <p>
-                    Welcome, {this.state.userDetails.givenName}!
-                  </p>
+                  <p>Welcome, {this.state.userDetails.givenName}!</p>
                 </div>
                 <div style={{ marginLeft: "5px" }}>
                   <Logout onSuccessfulLogout={this.handleSuccessfulLogout} />
@@ -58,24 +48,50 @@ class App extends Component {
           </div>
 
           <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            {this.state.isUserLoggedIn && <li><NavLink to="/projects">Projects</NavLink></li>}
-            {this.state.isUserLoggedIn && <li><NavLink to="/audioplayer">Audio Player</NavLink></li>}
-            {this.state.isUserLoggedIn && <li><NavLink to="/daw">DAW</NavLink></li>}
-
+            <li>
+              <NavLink exact to="/">
+                Home
+              </NavLink>
+            </li>
+            {this.state.isUserLoggedIn && (
+              <li>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+            )}
+            {this.state.isUserLoggedIn && (
+              <li>
+                <NavLink to="/audioplayer">Audio Player</NavLink>
+              </li>
+            )}
+            {this.state.isUserLoggedIn && (
+              <li>
+                <NavLink to="/daw">DAW</NavLink>
+              </li>
+            )}
           </ul>
 
           <div className="content">
             <Route exact path="/" component={Home} />
-            <PrivateRoute path="/projects" component={Projects} authed={this.state.isUserLoggedIn} />
-            <PrivateRoute path="/audioplayer" component={AudioPlayer} authed={this.state.isUserLoggedIn} />
-            <PrivateRoute path="/daw" component={DAW} authed={this.state.isUserLoggedIn} />
+            <PrivateRoute
+              path="/projects"
+              component={Projects}
+              authed={this.state.isUserLoggedIn}
+            />
+            <PrivateRoute
+              path="/audioplayer"
+              component={AudioPlayer}
+              authed={this.state.isUserLoggedIn}
+            />
+            <PrivateRoute
+              path="/daw"
+              component={DAW}
+              authed={this.state.isUserLoggedIn}
+            />
           </div>
 
           <Footer />
-
-        </div >
-      </HashRouter >
+        </div>
+      </HashRouter>
     );
   }
 }
